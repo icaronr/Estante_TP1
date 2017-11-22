b1a05c666a4b0c39f3b293a6805da1f4854d06d9
joaoadm94 <23438882+joaoadm94@users.noreply.github.com>
2017-11-22 00:12:46 -0200

    Update unidadePersistencia.h

--- unidadePersistencia.h
+++ unidadePersistencia.h
@@ -37,6 +37,9 @@ public:
 
 //---------------------------------------------------------------------------
 //Classe ElementoResultado.
+///A classe "ElementoResultado" recebe o retorno dos comandos que buscam valores no banco de dados.
+///Os métodos "setNomeColuna" e "setValorColuna" servem apenas para atribuir às strings "nomeColuna"
+///e "valorColuna" os valores recebidos do banco de dados.
 
 class ElementoResultado {
 private:
@@ -53,27 +56,51 @@ public:
 //---------------------------------------------------------------------------
 //Classe ComandoSQL.
 ///A classe "ComandoSQL" define os métodos de conexão do programa com o banco de dados gerado
-///com a utilização do SQLite. Os métodos privados "conectar" e "desconectar" realizam a tentativa
-///de abertura do banco de dados, retornando mensagem de erro em caso de falha.
+///com a utilização do SQLite.
 ///O método público "ComandoSQL" informa o nome do banco de dados.
-/// --- método executar ---
+///O método público "executar" realiza a execução dos diversos comandos SQL possíveis. O método
 
 class ComandoSQL {
 private:
+        /**
+        * String que armazena o nome do banco de dados.
+        */
         const char *nomeBancoDados;
+        /**
+        * Um ponteiro para banco de dados do SQLite3.
+        */
         sqlite3 *bd;
+        /**
+        * String que armazena mensagem de erro, se existente.
+        */
         char *mensagem;
         int rc;
+        /**
+         * Método que realiza a tentativa de abertura do banco de dados,
+         * retornando mensagem de erro em caso de falha.
+         */
         void conectar() throw (EErroPersistencia);
+        /**
+        * Método que realiza a tentativa de fechamento do banco de dados, retornando
+        * mensagem de erro em caso de falha.
+        */
         void desconectar() throw (EErroPersistencia);
+        
+        /*
+        * Método que recebe os elementos encontrados no banco de dados conforme a pesquisa realizada.
+        * Como parâmetros, recebe um inteiro que conta quantos elementos buscados foram encontrados,
+        * e dois ponteiros para string, um apontando para o nome da coluna e outro para os valores daquela coluna.
+        */
         static int callback(void *, int, char **, char **);
 
 protected:
         static list<ElementoResultado> listaResultado;
         string comandoSQL;
 public:
+        /*
+        * Informa o nome do banco de dados.
+        */
         ComandoSQL() {
-             //Informa o nome do banco de dados.
              nomeBancoDados = "bancoEstante.db";
         }
         void executar() throw (EErroPersistencia);
@@ -95,15 +122,27 @@ public:
 
 //---------------------------------------------------------------------------
 //Classe ComandoPesquisarUsuario.
+///Esta classe pesquisa um usuário no banco de dados por meio do apelido fornecido.
 
 class ComandoPesquisarUsuario:public ComandoSQL {
 public:
+        /*
+        * Este metódo utiliza um comando SQL para pesquisar um usuário no banco de dados a partir do apelido fornecido.
+        */
         ComandoPesquisarUsuario(Apelido);
+        /*
+        * Este metódo parte do resultado encontrado no método acima para preencher um objeto "usuario" com os dados
+        * encontrados no banco de dados, retornando o usuário pesquisado com todos os seus campos completos.
+        */
         Usuario getResultado() throw (EErroPersistencia);
 };
 
 //---------------------------------------------------------------------------
 //Classe ComandoCadastrarUsuario.
+/// Esta classe contém apenas o método "ComandoCadastrarUsuario(Usuario)", ou seja, recebe como parâmetro
+/// um objeto do tipo "usuario". Utiliza os métodos de acesso aos dados deste objeto (getApelido, 
+/// getSenha, getNome, getTelefone) e passa o resultado desses gets para o banco de dados por meio de comando
+/// INSERT do SQL, criando um novo usuário.  
 
 class ComandoCadastrarUsuario:public ComandoSQL {
 public:
